70706c14e55552f608e8cc78ab52c3be818940e9
Marco Avila <mavila6@email.arizona.com>
2021-10-06 04:09:41 -0700

    added create user/delete user/and create friend in controllers user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -46,6 +46,62 @@ const userController = {
             .then((data) => res.json(data))
             .catch((err) => res.status(400).json(err));
     },
+    // updates user 
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {
+            new: true,
+            runValidators: true,
+        })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: "User does not exist" });
+                    return;
+                }
+                res.json(data);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+    // deletes a user with id
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: "User does not exist" });
+                    return;
+                }
+                // could do res.json(data) but we don't need the data. We just need to know that it was deleted.
+                res.json(true);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+    // adds friend
+    createFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.id } },
+            { new: true }
+        ).then((data) => {
+            if (!data) {
+                res.status(404).json({ message: "No friend with this id" });
+                return;
+            }
+            User.findOneAndUpdate(
+                { _id: params.friendId },
+                { $addToSet: { friends: params.userId } },
+                { new: true }
+            )
+                .then((data) => {
+                    if (!data) {
+                        res.status(404).json({ message: "No friend with this id" });
+                    }
+                    res.json(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
+        });
+    },
 
 };
 
