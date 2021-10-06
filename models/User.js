298a64e38f153fecee60c112ae2ce4358de36039
Marco Avila <mavila6@email.arizona.com>
2021-10-06 02:37:17 -0700

    finished user.js models file

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-
+// 
 const UserSchema = new Schema({
     username: {
         type: String,
@@ -25,4 +25,22 @@ const UserSchema = new Schema({
             ref: 'User'
         }
     ]
+},
+{
+    toJSON: 
+    {
+        virtuals: true,
+    },
+    id: false
 });
+// returns length of the friends array
+UserSchema.virtual('friendCount').get(() => {
+    return this.friends.length;
+});
+//  returns length of thoughts array
+UserSchema.virtual('thoughtCount').get(() => {
+    return this.thoughts.length;
+});
+// out user schema in variable to be exported
+const User = model('User', UserSchema);
+module.exports = User;
