ba708487f6dd4a7bf0204123b7d5ae1b0999b88d
Marco Avila <mavila6@email.arizona.com>
2021-10-06 04:16:54 -0700

    added ability to get thoughts as well as by id and create new thoughts

---
+++ controllers/tought-controller.js
@@ -0,0 +1,61 @@
+const { User, Thought } = require("../models");
+
+const thoughtController = {
+    // gets all thoughts
+    getAllThoughts: (req, res) => {
+        Thought.find({})
+            .select("-__v")
+            .sort({ _id: -1 })
+            .then(data => res.json(data))
+            // logs errors
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+    // retrieves thoughts by id
+    getThoughtById(req, res) {
+        Thought.findOne({ _id: params.id })
+            .populate({
+                path: "thought",
+                select: "-__v",
+            })
+            .populate({
+                path: "reactions",
+                select: "-__v",
+            })
+            .select("-__v")
+            .then((data) => {
+                console.log(data);
+                if (!data) {
+                    res.status(404).json({ message: "Thought does not exist" });
+                }
+                res.json(data);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // creates a new thought
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then(({ __id }) => {
+                return User.findOneAndUpdate(
+                    { _id: req.params.userId },
+                    { $push: { thoughts: __id } },
+                    { new: true }
+                );
+            })
+            .then((data) => {
+                if (!data) {
+                    return res.status(404).json({ message: "Does not match an ID" });
+                }
+                res.json(data);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json();
+            });
+    }
+}
